24d7ea750274be97d0f1a259d4960d0f77492a28
Steven Montero <steven.monteromunoz@ucrso.info>
2020-11-26 21:48:57 -0600

    Fix error

--- lib/utils/regularExpressions/no_emty.dart
+++ lib/utils/regularExpressions/no_emty.dart
@@ -2,9 +2,9 @@ import 'package:formz/formz.dart';
 
 enum EmptyValidationError { invalid }
 
-class EmptyValidator extends FormzInput<DateTime, EmptyValidationError> {
-  const EmptyValidator.pure() : super.pure(null);
-  const EmptyValidator.dirty([DateTime value]) : super.dirty(value);
+class EmptyDateValidator extends FormzInput<DateTime, EmptyValidationError> {
+  const EmptyDateValidator.pure() : super.pure(null);
+  const EmptyDateValidator.dirty([DateTime value]) : super.dirty(value);
   @override
   EmptyValidationError validator(DateTime value) {
     return value != null ? null : EmptyValidationError.invalid;
